fa31d4b06b836da25416a838c17b24b028622262
gildydtjd <gildydtjd@naver.com>
2021-04-30 23:13:02 +0900

    #Catch 구조짜기

--- src/page/Main.js
+++ src/page/Main.js
@@ -3,157 +3,78 @@ import styled from "styled-components";
 import useScrollFadeIn from "../hooks/useScrollFadeIn";
 function Main(props) {
   const SectionBox = styled.div`
-    @media only screen and (max-width: 480px) {
-      display: block;
-      background-position-x: center;
-
-      .sectionBox__left {
-        width: 100% !important;
-        height: 70vh !important;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-      }
-      .sectionBox__left__info {
-        height: 300px !important;
-        background-size: cover;
-        width: 300px !important;
-      }
-      .sectionBox__right {
-        width: 100% !important;
-        height: 30vh !important;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-      .sectionBox__right__info {
-        margin: 0;
-        font-size: 20px !important;
-        text-align: center;
-      }
-    }
     width: 100%;
     height: 100vh;
-    justify-content: center;
-    align-items: center;
-    background-color: #000000;
-    margin-top: -4px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
     background-image: url(${(props) => props.url});
     background-size: cover;
     background-repeat: no-repeat;
-    .sectionBox__left {
-      width: 50%;
-      height: 100vh;
-      .sectionBox__left__info {
-        background-image: url(${(props) => props.imgUrl});
-        background-size: cover;
-        background-repeat: no-repeat;
-        width: 80%;
-        height: 80%;
-        margin: 10%;
-        border-radius: 5px;
+    opacity: ${(props) => props.opa};
+
+    .sectionBox__header {
+      width: 100%;
+      display: flex;
+      justify-content: flex-end;
+      span {
+        color: white;
+        padding: 30px 20px;
+        font-size: 22px;
+        margin-right: 100px;
+        font-weight: 800;
+        :hover {
+          color: gold;
+          cursor: pointer;
+        }
       }
     }
-    .sectionBox__right {
-      width: 50%;
-      height: 100vh;
-      background-color: black;
-      .sectionBox__right__info {
-        color: #fff;
-        width: 80%;
-        margin: 70% ${(props) => props.p};
-        font-size: 30px;
+
+    .sectionBox__sub {
+      height: 50vh;
+      background-color: green;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .sectionBox__sub__info {
+        background-color: pink;
+        width: 400px;
+        height: 400px;
+        margin: 20px;
+      }
+    }
+    .sectionBox__sub2 {
+      height: 50vh;
+      background-color: aqua;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      .sectionBox__sub2__info {
+        background-color: wheat;
+        width: 800px;
+        height: 430px;
       }
     }
   `;
 
   return (
     <>
-      <SectionBox url="/assets/img/cat7.jpg" />
-      <SectionBox imgUrl="/assets/img/cat5.jpg" p="8%">
-        <div className="sectionBox__left">
-          <div
-            className="sectionBox__left__info"
-            {...useScrollFadeIn("right", 2.5, 0.4)}
-          ></div>
-        </div>
-        <div className="sectionBox__right">
-          <div
-            className="sectionBox__right__info"
-            {...useScrollFadeIn("left", 2.5, 0.5)}
-          >
-            부모님은 고양이를 싫어하셨다
-          </div>
+      <SectionBox url="/assets/img/cat7.jpg" opa="1">
+        <div className="sectionBox__header">
+          <span {...useScrollFadeIn("left", 1, 0.3)}>HOME</span>
+          <span {...useScrollFadeIn("left", 1, 0.4)}>ABOUT</span>
+          <span {...useScrollFadeIn("left", 1, 0.5)}>SHOP</span>
+          <span {...useScrollFadeIn("left", 1, 0.6)}>Q&A</span>
         </div>
       </SectionBox>
-      <SectionBox imgUrl="/assets/img/cat3.jpg" p="25%">
-        <div className="sectionBox__right">
-          <div
-            className="sectionBox__right__info"
-            {...useScrollFadeIn("right", 2.5, 0.4)}
-          >
-            그래서 나는 고양이를 데려왔다
-          </div>
+      <SectionBox>
+        <div className="sectionBox__sub">
+          <div className="sectionBox__sub__info"></div>
+          <div className="sectionBox__sub__info"></div>
+          <div className="sectionBox__sub__info"></div>
         </div>
-        <div className="sectionBox__left">
-          <div
-            className="sectionBox__left__info"
-            {...useScrollFadeIn("left", 2.5, 0.5)}
-          ></div>
+        <div className="sectionBox__sub2">
+          <div className="sectionBox__sub2__info"></div>
         </div>
       </SectionBox>
-      <SectionBox imgUrl="/assets/img/cat2.jpg" p="8%">
-        <div className="sectionBox__left">
-          <div
-            className="sectionBox__left__info"
-            {...useScrollFadeIn("right", 2.5, 0.4)}
-          ></div>
-        </div>
-        <div className="sectionBox__right">
-          <div
-            className="sectionBox__right__info"
-            {...useScrollFadeIn("left", 2.5, 0.5)}
-          >
-            집에 온지 어느덧 5년...
-          </div>
-        </div>
-      </SectionBox>
-      <SectionBox imgUrl="/assets/img/cat6.jpg" p="25%">
-        <div className="sectionBox__right">
-          <div
-            className="sectionBox__right__info"
-            {...useScrollFadeIn("right", 2.5, 0.4)}
-          >
-            어머니가 말씀하셨다. <br />
-            너없인 살아도 냥냥이 없인 못산다고..
-          </div>
-        </div>
-        <div className="sectionBox__left">
-          <div
-            className="sectionBox__left__info"
-            {...useScrollFadeIn("left", 2.5, 0.5)}
-          ></div>
-        </div>
-      </SectionBox>
-      <SectionBox imgUrl="/assets/img/cat8.jpg" p="8%">
-        <div className="sectionBox__left">
-          <div
-            className="sectionBox__left__info"
-            {...useScrollFadeIn("right", 2.5, 0.4)}
-          ></div>
-        </div>
-        <div className="sectionBox__right">
-          <div
-            className="sectionBox__right__info"
-            {...useScrollFadeIn("left", 2.5, 0.5)}
-          >
-            고양이는 사랑이다 ❤
-          </div>
-        </div>
-      </SectionBox>
+      <SectionBox></SectionBox>
     </>
   );
 }
