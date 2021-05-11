933f5074e91fb1f731b6860f22dcced1c3a6762b
gildydtjd <gildydtjd@naver.com>
2021-05-11 20:04:33 +0900

    #Company

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import "./App.css";
 import { Route, BrowserRouter } from "react-router-dom";
 import Main from "./page/Main";
-// import About from "./page/About";
-// import Shop from "./page/Shop";
-// import QNA from "./page/QNA";
-
+import About from "./page/About";
+import Shop from "./page/Shop";
+import QNA from "./page/QNA";
+import Company from "./page/Company";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Route exact path="/" component={Main} />
-        <Route path="/company" component={Main} />
-        <Route path="/about" component={Main} />
-        <Route path="/shop" component={Main} />
-        <Route path="/qna" component={Main} />
+        <Route path="/company" component={Company} />
+        <Route path="/about" component={About} />
+        <Route path="/shop" component={Shop} />
+        <Route path="/qna" component={QNA} />
       </BrowserRouter>
     </div>
   );
--- src/page/Company.js
+++ src/page/Company.js
@@ -1,7 +1,31 @@
 import React from "react";
-
+import styled from "styled-components";
 function Company(props) {
-  return <div></div>;
+  const SectionBox = styled.div`
+    width: 100%;
+    height: ${(props) => (props.height ? props.height : "100vh")};
+    background-image: url(${(props) => props.url});
+    background-size: cover;
+    background-repeat: no-repeat;
+    opacity: ${(props) => props.opa};
+    display: ${(props) => (props.dis ? props.dis : "flex")};
+    align-items: center;
+    justify-content: center;
+    background-position: center;
+    .company__about {
+      background-color: green;
+      width: 50%;
+      height: 50%;
+    }
+  `;
+
+  return (
+    <>
+      <SectionBox url="/assets/img/cat13.jpg">
+        <div className="company__about"></div>
+      </SectionBox>
+    </>
+  );
 }
 
 export default Company;
