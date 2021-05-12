49c25870c620fb37af3e7b51e074923aea6dcf48
gildydtjd <gildydtjd@naver.com>
2021-05-12 23:32:32 +0900

    #About page

--- src/page/About.js
+++ src/page/About.js
@@ -1,7 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
 function About(props) {
-  return <div></div>;
+  const SectionBox = styled.div`
+    width: 100%;
+    height: ${(props) => (props.height ? props.height : "100vh")};
+    background-image: url(${(props) => props.url});
+    background-size: cover;
+    background-repeat: no-repeat;
+    opacity: ${(props) => props.opa};
+    display: ${(props) => (props.dis ? props.dis : "flex")};
+    align-items: center;
+    justify-content: center;
+    background-position: center;
+  `;
+  return (
+    <>
+      <SectionBox url="/assets/img/cat19.jpg"></SectionBox>
+    </>
+  );
 }
 
 export default About;
