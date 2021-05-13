df0e2cd7ad69f56e0d2b637b39be6ccaace47c6b
gildydtjd <gildydtjd@naver.com>
2021-05-13 23:37:40 +0900

    #about page

--- src/page/About.js
+++ src/page/About.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import Header from "../components/header/Header";
+import useScrollFadeIn from "../hooks/useScrollFadeIn";
 
 function About(props) {
   const SectionBox = styled.div`
@@ -11,12 +13,36 @@ function About(props) {
     opacity: ${(props) => props.opa};
     display: ${(props) => (props.dis ? props.dis : "flex")};
     align-items: center;
-    justify-content: center;
+    color: #fff;
+
     background-position: center;
+    .about__info {
+      width: 100%;
+      text-align: center;
+      height: 80%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      h2 {
+        font-size: 150px;
+        text-shadow: 10px 5px 10px #000000;
+      }
+    }
   `;
   return (
     <>
-      <SectionBox url="/assets/img/cat19.jpg"></SectionBox>
+      <SectionBox url="/assets/img/cat/cat19.jpg" dis="block">
+        <Header />
+        <div className="about__info" {...useScrollFadeIn("right", 1, 0.5)}>
+          <h2 style={{ color: "red" }}>A</h2>
+          <h2 style={{ color: "yellow" }}>b</h2>
+          <h2 style={{ color: "blue" }}>o</h2>
+          <h2 style={{ color: "orange" }}>u</h2>
+          <h2 style={{ color: "aqua" }}>t</h2>
+          <h2 style={{ color: "green" }}>!</h2>
+        </div>
+      </SectionBox>
+      <SectionBox></SectionBox>
     </>
   );
 }
