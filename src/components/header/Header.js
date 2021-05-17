2209c1e91b585792a58b0d687bcae5b312e0c2fd
gildydtjd <gildydtjd@naver.com>
2021-05-17 22:44:51 +0900

    #header css

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,7 +10,7 @@ function Header(props) {
     position: sticky;
     top: 0;
     z-index: 99;
-    background-color: #00001090;
+    background-color: rgba(0, 0, 0, 0.565);
     .sectionBox__header__link {
       color: white;
       padding: 30px 20px;
